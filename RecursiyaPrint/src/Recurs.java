e2e2aaa5a6a93d97add64a02daf7f2d9070e60a0
Alexandr Orlichenko <orlichenko1975@gmail.com>
2018-09-15 11:08:17 +0300

    recursion ver1

---
+++ RecursiyaPrint/src/Recurs.java
@@ -0,0 +1,11 @@
+
+class Recurs {
+	
+	void rec(int n) {
+			if (n > 1)
+				rec (n - 1);
+			System.out.print(n + " ");
+	}
+}
+
+
---
+++ RecursiyaPrint/src/RecursiyaPrint.java
@@ -0,0 +1,7 @@
+
+public class RecursiyaPrint {
+	public static void main(String[] args) {
+		Recurs r = new Recurs();
+		r.rec(20);
+	}
+}
