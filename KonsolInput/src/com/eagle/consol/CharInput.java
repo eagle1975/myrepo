6eceadcc799ecb887fa80d9baaf9dc882df9c8dd
Alexandr Orlichenko <orlichenko1975@gmail.com>
2018-10-22 09:17:59 +0300

    Changed the language of explanations

--- KonsolInput/src/com/eagle/consol/CharInput.java
+++ KonsolInput/src/com/eagle/consol/CharInput.java
@@ -7,9 +7,9 @@ public class CharInput {
 		try {
 			x = System.in.read();
 			char c = (char)x;
-			System.out.println("Код символа " + c +" = " + x);
+			System.out.println("Character code " + c +" = " + x);
 		} catch (java.io.IOException e) {
-			System.err.println("Ошибка ввода" + e);
+			System.err.println("Input Error" + e);
 			
 		}
 	}
