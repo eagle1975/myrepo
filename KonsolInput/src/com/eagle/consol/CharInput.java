57efc8fe91ba22ba93027ea08afd19d360dafda1
Alexandr Orlichenko <orlichenko1975@gmail.com>
2018-10-22 08:44:07 +0300

    Lerning konsole input

---
+++ KonsolInput/src/com/eagle/consol/CharInput.java
@@ -0,0 +1,17 @@
+package com.eagle.consol;
+
+public class CharInput {
+
+	public static void main(String[] args) {
+		int x;
+		try {
+			x = System.in.read();
+			char c = (char)x;
+			System.out.println("Код символа " + c +" = " + x);
+		} catch (java.io.IOException e) {
+			System.err.println("Ошибка ввода" + e);
+			
+		}
+	}
+
+}
