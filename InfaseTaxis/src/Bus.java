fe2d3ba3a488d008c685c9f059846eff0013bf71
--replace-all <orlichenko1975@gmail.com>
2019-01-27 20:35:10 +0200

    Сreated an interface Vehicle

---
+++ InfaseTaxis/src/Bus.java
@@ -0,0 +1,31 @@
+// 08.01.2019
+// Taxi Park. Cost calculation. Sort by fuel consumption. Finding cars at maximum speed in a given range.
+public class Bus extends PassengerImpl {
+	
+	private String purpose;
+	
+	public Bus(int id, String mark, String model, String color, int yearProduction, float price, float fuelConsumption,
+			int maxSpeed, int numberOfPassengers, String purpose) {
+		super(id, mark, model, color, yearProduction, price, fuelConsumption, maxSpeed, numberOfPassengers);
+		
+		this.purpose = purpose;
+	}
+
+	public String getPurpose() {
+		return purpose;
+	}
+
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+
+	@Override
+	public String toString() {
+		return "Bus [Id=" + getId() + ", Mark=" + getMark() + ", Model=" + getModel() + ", Color="
+				+ getColor() + ", YearProduction=" + getYearProduction() + ", Price=" + getPrice()
+				+ ", FuelConsumption=" + getFuelConsumption() + ", MaxSpeed=" + getMaxSpeed()
+				+ ", NumberOfPassengers=" + getNumberOfPassengers() + ", purpose=" + purpose + "]";
+	}
+
+	
+}
