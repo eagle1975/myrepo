b9dc125453f51446f3f208b0a6eb4d2bcf80773d
--replace-all <orlichenko1975@gmail.com>
2019-02-16 09:11:39 +0200

    left one exception handling - IOException

--- I_OTaxis/src/com/test/iotax/main/DataFileReader.java
+++ I_OTaxis/src/com/test/iotax/main/DataFileReader.java
@@ -21,8 +21,6 @@ public class DataFileReader {
 			int n = fin.available();
 			byteBufer = new byte[n];
 			fin.read(byteBufer);
-		} catch(FileNotFoundException e) {
-			System.err.println("Open file error!");
 		} catch(IOException e) {
 			System.err.println("Error read from file!");
 		} finally {
@@ -39,17 +37,20 @@ public class DataFileReader {
 				for (int i = 0; i < vehicle.length; i++) {
 					vehicleProperties = vehicle[i].split(",");
 					switch (vehicleProperties[vehicleProperties.length - 1].trim()) {
-						case  "cargo" : resaltArray[i] = new CargoTaxiImpl(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(), vehicleProperties[2].trim(), vehicleProperties[3].trim(),
-								Integer.parseInt(vehicleProperties[4].trim()), Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()),
-								Integer.parseInt(vehicleProperties[7].trim()), Integer.parseInt(vehicleProperties[8].trim()));
+						case  "cargo" : resaltArray[i] = new CargoTaxiImpl(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(), 
+								vehicleProperties[2].trim(),  vehicleProperties[3].trim(), Integer.parseInt(vehicleProperties[4].trim()),
+								Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()), Integer.parseInt(vehicleProperties[7].trim()),
+								Integer.parseInt(vehicleProperties[8].trim()));
 							break;
-						case "bus" :  resaltArray[i] = new Bus(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(), vehicleProperties[2].trim(), vehicleProperties[3].trim(),
-								Integer.parseInt(vehicleProperties[4].trim()), Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()),
-								Integer.parseInt(vehicleProperties[7].trim()), Integer.parseInt(vehicleProperties[8].trim()), vehicleProperties[9].trim());
+						case "bus" :  resaltArray[i] = new Bus(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(),
+								vehicleProperties[2].trim(), vehicleProperties[3].trim(), Integer.parseInt(vehicleProperties[4].trim()),
+								Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()), Integer.parseInt(vehicleProperties[7].trim()),
+								Integer.parseInt(vehicleProperties[8].trim()), vehicleProperties[9].trim());
 							break;
-						case "car" : resaltArray[i] = new Cars(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(), vehicleProperties[2].trim(), vehicleProperties[3].trim(),
-								Integer.parseInt(vehicleProperties[4].trim()), Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()),
-								Integer.parseInt(vehicleProperties[7].trim()), Integer.parseInt(vehicleProperties[8].trim()), vehicleProperties[9].trim());
+						case "car" : resaltArray[i] = new Cars(Integer.parseInt(vehicleProperties[0]), vehicleProperties[1].trim(),
+								vehicleProperties[2].trim(), vehicleProperties[3].trim(), Integer.parseInt(vehicleProperties[4].trim()),
+								Integer.parseInt(vehicleProperties[5].trim()), Integer.parseInt(vehicleProperties[6].trim()), Integer.parseInt(vehicleProperties[7].trim()),
+								Integer.parseInt(vehicleProperties[8].trim()), vehicleProperties[9].trim());
 							break;
 					}
 				}	
